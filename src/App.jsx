43aabef634e437a8a931114e1f55b4d3457e236e
CLSnapp <repbandito@hotmail.com>
2024-11-14 20:40:58 -0600

    Use of map and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import ContactList from "./components/ContactList";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="card">
+    <ContactList />
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div>
+      </div> */}
+
     </>
   );
 }
